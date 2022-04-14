b0ea7cb8457c8d55c042979295cc8f4147258ba4
Ferhat <61943812+Ferhat-Sindy@users.noreply.github.com>
2022-04-14 16:57:57 +0200

    working 2d problem with nonuniform grid

--- regular_grid_FV_solver/2D_nonlinear.jl
+++ regular_grid_FV_solver/2D_nonlinear.jl
@@ -7,10 +7,13 @@ using GLMakie
 using Sundials
 using NLsolve
 using BenchmarkTools
+using TimerOutputs
+using DelimitedFiles
+include("plotting.jl")
 
 function new_assemble_nonlinear_system(gridX,gridY,bc_bott::Number,bc_top::Number)
-    N = size(gridX)
-    λ = 1
+    N = (n_y,n_x)
+    λ = 0.01
     c = 0.001
     α = 0.01
     k = 100.0
@@ -30,7 +33,7 @@ function new_assemble_nonlinear_system(gridX,gridY,bc_bott::Number,bc_top::Numbe
                 u1[i,j] = u[i-1,j]-u[i,j] 
             end    
         end
-        return u1 ./ h_left
+        return u1 ./ h_top
     end
 
     function ∇_right(u)
@@ -40,7 +43,7 @@ function new_assemble_nonlinear_system(gridX,gridY,bc_bott::Number,bc_top::Numbe
                 u1[i,j] = u[i+1,j]-u[i,j] 
             end
         end
-        return u1 ./ h_right
+        return u1 ./ h_bottom
     end
 
     function ∇_bottom(u)
@@ -50,7 +53,7 @@ function new_assemble_nonlinear_system(gridX,gridY,bc_bott::Number,bc_top::Numbe
                 u1[i,j] = u[i,j-1]-u[i,j] 
             end    
         end
-        return u1 ./ h_bottom
+        return u1 ./ h_left
     end
 
     function ∇_top(u)
@@ -60,7 +63,7 @@ function new_assemble_nonlinear_system(gridX,gridY,bc_bott::Number,bc_top::Numbe
                 u1[i,j] = u[i,j+1]-u[i,j] 
             end
         end
-        return u1 ./ h_top
+        return u1 ./ h_right
     end
 
     function L(u)
@@ -147,34 +150,39 @@ function newton(A,b,u0; tol=1.0e-8, maxit=100)
     throw("convergence failed")
 end
 
-n = 30
-n_fine = n÷3
-n_coarse = n - n_fine
-grid1y = LinRange(0.0,10.0,n_fine)
-grid2y = LinRange(10.0,150.0,n_coarse+1)[2:end]
-#gridy = reverse(vcat(grid1y,grid2y)) Uncomment this for nonuniform grid but also comment uniform one
-gridx = LinRange(0,300.0,n)
-gridy = reverse(LinRange(0,150.0,n)) #uniform grid
+n_x = 10
+n_y = 100
+n_fine = n_y÷3
+n_coarse = n_y - n_fine
+grid1y = LinRange(0.0,2.0,n_fine)
+grid2y = LinRange(2.0,150.0,n_coarse+1)[2:end]
+gridy = reverse(vcat(grid1y,grid2y)) #Uncomment this for nonuniform grid but also comment uniform one (does not work yet)
+gridx = LinRange(0,300.0,n_x)
+#gridy = reverse(LinRange(0,150.0,n_y)) #uniform grid
 
-gridX = gridx'.*ones(n_fine+n_coarse)
-gridY = ones(n)'.*gridy
+gridX = gridx'.*ones(n_y)
+gridY = ones(n_x)'.*gridy
 
 bc_bott = 0.5
 bc_top = 0.0
 t_end = 10.0
 
+#defining system
 sys = new_assemble_nonlinear_system(gridX,gridY,bc_bott,bc_top)
-x_T_0 = zeros(Float64,(n,n))
-x_p_0 = zeros(Float64,n*n)
-x_T_0[end,:] .= bc_bott
-x_T_0 = reshape(x_T_0,n*n)
-x_0 = vcat(x_T_0,x_p_0)
-b = zeros(Float64,n*n*2)
 
+#creating initial guess for steady_state_solution
+x_T_0 = zeros(Float64,(n_y,n_x))
+x_p_0 = zeros(Float64,n_x*n_y)
+x_T_0[end,:] .= bc_bott
+x_T_0 = reshape(x_T_0,n_x*n_y)
+x_0 = vcat(x_T_0,x_p_0)
+b = zeros(Float64,n_x*n_y*2)
+
+#calculating steady_state_solution with newtons method
 steady_state_solution,res = newton(sys,b,x_0)
 
-Temperature = reshape(steady_state_solution[1:n*n],(n,n))
-pressure = reshape(steady_state_solution[n*n+1:end],(n,n))
+Temperature = reshape(steady_state_solution[1:n_x*n_y],(n_y,n_x))
+pressure = reshape(steady_state_solution[n_x*n_y+1:end],(n_y,n_x))
 
 fig = Figure()
 println("Solution obtained, start plotting")
@@ -183,6 +191,100 @@ hm = surface!(ax,gridX, gridY, Temperature )
 Colorbar(fig[1, 2],hm )
 display(fig)
 
-#@code_warntype assemble_nonlinear_system(gridX,gridY,bc_bott,bc_top)
-#@code_warntype sys(x_0)
-#sys(x_0)
+###TIMEDEPENDENT_PROBLEM###
+"""
+Creates a DAE system f(du,u,p,t) = 0
+"""
+function create_DAE_function(f,n) 
+    E = spdiagm(0 => vcat(ones(Float64,n),zeros(Float64,n)))
+    
+    function DAE(du,u,p,t)
+        return E*du - f(u)
+    end
+    
+    return DAE
+end
+
+function create_constraint_function(f,x)
+    n = length(x)
+    
+    function a(y)
+        z = vcat(x,y)
+        return f(z)[n+1:end]
+    end
+    
+    return a
+end
+
+function get_time_solution(f, T_0, time_interval)
+
+    println("Solver initialized")
+
+    @timeit to "system solution" begin
+        n   = length(T_0) 
+        DAE = create_DAE_function(f,n)     
+        g   = create_constraint_function(f, T_0)
+
+        write(stdin.buffer, 0x0C)
+        println("\t Computing consistent initial conditions for pressure") 
+        @timeit to "newton's method" P_0 = newton(g,zeros(Float64,n),zeros(Float64,n); tol=1.0e-8, maxit=100)[1]
+        u0  = vcat(T_0, P_0)
+        du0 = f(u0)
+
+        println("\n\t Solving DAE...")
+
+        @timeit to "DAE solver" begin
+            @timeit to "assmebling" prob = DAEProblem(DAE, du0, u0, time_interval)
+            @timeit to "solving" sol  = solve(prob, IDA(), dt=0.1; verbose = true)
+        end
+        
+        println("\nSolution available. Summary and solution:\n")
+
+        return sol
+    end
+end
+
+# Create the timer object
+to = TimerOutput()
+
+function headingText()
+    println("
+            ╔═══╦═══╦═══╦═══╦═══╗
+            ║╔═╗║╔═╗║╔═╗║╔═╗║╔══╝
+            ║║─╚╣║─║║╚══╣╚══╣╚══╗
+            ║║─╔╣║─║╠══╗╠══╗║╔══╝
+            ║╚═╝║╚═╝║╚═╝║╚═╝║╚══╗
+            ╚═══╩═══╩═══╩═══╩═══╝\n")
+
+    println("FVM-based solver of a heat and mass transfer problem in a porous medium\n")
+    println("Grid size: ($(n_x) x $(n_y))")
+    println("Temporal domain: (0, $(t_end)) s \n")
+end
+
+headingText()
+
+T_0 = x_T_0
+t_end = 1
+solution = get_time_solution(sys, T_0, (0.0,t_end));
+
+display(solution)
+println("\n Performance report:")
+display(to)
+
+solution_at_t = solution(0.01)
+
+Temperature = reshape(solution_at_t[1:n_x*n_y],(n_y,n_x))
+pressure = reshape(solution_at_t[n_x*n_y+1:end],(n_y,n_x))
+
+fig = Figure()
+println("Solution obtained, start plotting")
+ax = Axis3(fig[1,1]; aspect=(1, 1, 1),xlabel = "x",ylabel = "y",zlabel = "pressure")
+hm = surface!(ax,gridX, gridY, Temperature )
+Colorbar(fig[1, 2],hm )
+display(fig)
+
+# Work in progress. In the meanwhile, I'll export the solution as csv and animate the plot in Python (easiest solution)
+#
+# animate_solution(grid, solution, nx; specie = "temperature", projection = "2d")
+
+writedlm("solution.csv",  solution, ',')
