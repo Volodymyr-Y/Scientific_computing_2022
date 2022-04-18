118adb0487131f8a4a69847c4c85014687427b14
Ferran <deandresvertferran@gmail.com>
2022-04-18 12:42:22 +0200

    post process (#4)

--- regular_grid_FV_solver/1D_nonlinear.jl
+++ regular_grid_FV_solver/1D_nonlinear.jl
@@ -356,7 +356,7 @@ grid2 = LinRange(2.0,150.0,n_coarse+1)[2:end]
 grid  = vcat(grid1,grid2)
 
 bc_top  = 0.0
-bc_bott = 2.0
+bc_bott = 0.0
 t_end   = 10.0
 
 x_0     = zeros(Float64, n*2)
@@ -388,6 +388,9 @@ end
 T_0 = LinRange(bc_bott,0.0,n)
 time_solution = get_time_solution(sys!,T_0, (0.0,t_end))
 
+T      = zeros((n, nx))
+P      = zeros((n, nx)) 
+
 
 println("\t Done. Calculating the water mass flux.")
 # Compute and plot flux
@@ -406,11 +409,13 @@ display(time_solution)
 println("\n Performance report:")
 display(to)
 
-plot_flux(xx, yy,fluxY, fluxX; save = true)
+# plot_flux(xx, yy,fluxY, fluxX; save = true)
 
 # plot_results(grid, solution(0), nx; savepdf = true, projection = "3d")
 
 # Work in progress. In the meanwhile, I'll export the solution as csv and animate the plot in Python (easiest solution)
 #
 # animate_solution(grid, solution, nx; specie = "temperature", projection = "2d")
-# writedlm("T_2.csv",  time_solution, ',')
+# writedlm("lambda_10.csv",  time_solution(10), ',')
+
+plot_results(grid, time_solution(1), nx, "_steady_homogeneous"; savepdf = true, projection = "3d")
